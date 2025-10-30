fd58cfc76eae0349145773216233ea84f004e228
Wai Daniel <waidanielchol478@gmail.com>
2025-10-30 22:28:16 +0300

    added the url

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ const corsOptions = {
       "https://nileflow-com.onrender.com",
       "http://localhost:3000", // Add if you have frontend dev server on 3000
       "https://nileflow.co.ke",
+      "https://nileassitbackend.onrender.com",
     ];
 
     // Add the clientUrl if it's set and not already in the list
